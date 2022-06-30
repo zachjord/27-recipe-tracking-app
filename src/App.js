feacb1ef737f70064175bd5ecdcf02983e919415
Zachary Jordan <zacjor1@gmail.com>
2022-06-29 19:54:52 -0500

    pass recipes to RecipeList

--- src/App.js
+++ src/App.js
@@ -10,11 +10,10 @@ function App() {
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
   // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
 
-  
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
+      <RecipeList recipes={recipes} />
       <RecipeCreate />
     </div>
   );
