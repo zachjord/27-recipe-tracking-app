cfaed629bd0bb47ae81f25699c2e57907f273f71
Zachary Jordan <zacjor1@gmail.com>
2022-06-29 19:57:47 -0500

    RecipeList now lists all recipes passed to it

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,26 +1,43 @@
 import React from "react";
 
-function RecipeList() {
-  
+function RecipeList({ recipes }) {
+
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
-  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
 
-  return (
-    <div className="recipe-list">
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-        
-        </tbody>
-      </table>
-    </div>
-  );
+return (
+  <div className="recipe-list" onSubmit={handleDelete}>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Cuisine</th>
+          <th>URL</th>
+          <th>Ingredients</th>
+          <th>Preparation</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+
+  {recipes.map((recipe, index) => {
+    return (
+    <tbody key={index}>
+      <tr>
+        <td>{recipe.name}</td>
+        <td>{recipe.cuisine}</td>
+        <td><img src={recipe.photo} alt="placeholder" /></td>
+        <td>{recipe.ingredients}</td>
+        <td>{recipe.preparation}</td>
+        <td><button name="delete">Delete</button></td>
+      </tr>
+    </tbody>
+    );
+  })}
+
+  </table>
+</div>
+);
+
 }
 
 export default RecipeList;
