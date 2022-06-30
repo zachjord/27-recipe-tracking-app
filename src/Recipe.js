c7f30b87abb8d8bffed05983220f685ff55e7b28
Zachary Jordan <zacjor1@gmail.com>
2022-06-30 16:54:07 -0500

    add delete functionality

---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function Recipe({ name, cuisine, photo, ingredients, preparation, index, handleDelete }) {
+
+  return (
+    <tbody key={index}>
+      <tr>
+        <td>{name}</td>
+        <td>{cuisine}</td>
+        <td><img src={photo} alt={name} /></td>
+        <td className="content_td"><p>{ingredients}</p></td>
+        <td className="content_td"><p>{preparation}</p></td>
+        <td><button name="delete" id={index} onClick={handleDelete}>Delete</button></td>
+      </tr>
+    </tbody>
+  );
+}
+export default Recipe;
