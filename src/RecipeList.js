12de38fa1bb26a31a5dd34eb80e1bc89c061e233
Zachary Jordan <zacjor1@gmail.com>
2022-06-30 11:20:22 -0500

    Changed "URL" to "Photo"

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -20,7 +20,7 @@ return (
         <tr>
           <th>Name</th>
           <th>Cuisine</th>
-          <th>URL</th>
+          <th>Photo</th>
           <th>Ingredients</th>
           <th>Preparation</th>
           <th>Actions</th>
