4bcaa17b14459b138355dc58dec8cd00f454ca69
Zachary Jordan <zacjor1@gmail.com>
2022-06-29 19:56:01 -0500

    added non-functionl form

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -5,13 +5,27 @@ function RecipeCreate() {
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
-  
+
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+            <input id="name" name="name" size="18" placeholder="Name" />
+            </td>
+            <td>
+            <input id="cuisine" name="cuisine" size="18" placeholder="Cuisine" />
+            </td>
+            <td>
+            <input id="photo" name="photo" size="18" type="url" placeholder="URL" />
+            </td>
+            <td>
+            <textarea id="ingredients" name="ingredients" size="18" placeholder="Ingredients" />
+            </td>
+            <td>
+            <textarea id="preparation" name="preparation" size="18" placeholder="Preparation" />
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
